8f7da553db872f785d73f8f3bcb306e721e77ead
denfad <denfad2003@mail.ru>
2023-06-30 21:29:16 +0300

    Migration to MongoDb

--- src/main/java/ru/denfad/UrlShortener/repository/UrlRepository.java
+++ src/main/java/ru/denfad/UrlShortener/repository/UrlRepository.java
@@ -1,9 +1,7 @@
 package ru.denfad.UrlShortener.repository;
 
+import ru.denfad.UrlShortener.model.UrlDocument;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-import ru.denfad.UrlShortener.model.UrlModel;
-
-public interface UrlRepository extends JpaRepository<UrlModel, Integer> {
+public interface UrlRepository {
+    UrlDocument readAndUpdateUrl(int id);
 }
